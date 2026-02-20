9da1b04b3ef96c34001f493acaf58912e907e555
IQUXAe <iquxae@proton.me>
2026-02-20 13:47:01 +0300

    ANDROID: fs: replace unsafe strncpy usages with strscpy/strscpy_pad
    Using strscpy internally reduces zero-padding overhead and prevents
    potential bounds bugs. In cases where the destination is a fixed-size
    disk struct or user-facing struct, strscpy_pad is used to safely pad
    and prevent information leaks while providing bounds checking.
    
    Modified files:
    - fs/exec.c (__get_task_comm)
    - fs/ext4/super.c (save_error_info)
    - fs/squashfs/namei.c (get_dir_index_using_name)

--- fs/exec.c
+++ fs/exec.c
@@ -1231,7 +1231,7 @@ killed:
 char *__get_task_comm(char *buf, size_t buf_size, struct task_struct *tsk)
 {
 	task_lock(tsk);
-	strncpy(buf, tsk->comm, buf_size);
+	strscpy(buf, tsk->comm, buf_size);
 	task_unlock(tsk);
 	return buf;
 }
